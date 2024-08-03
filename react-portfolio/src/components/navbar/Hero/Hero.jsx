d0718779bae0cb5b3f53c0a7a634db9489e55817
Zuwarashe <zuwarashem1998@gmail.com>
2024-08-03 03:53:49 +0200

    better hero hahahaha

--- react-portfolio/src/components/navbar/Hero/Hero.jsx
+++ react-portfolio/src/components/navbar/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
     <section className={styles.container}>
       <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
       <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I'm Zuwarashe</h1>
+        <h1 className={styles.title}>Hi, I'm Zuwa</h1>
         <h3 className={styles.textAnimation}>I'm a <span></span></h3>
         <TextHeroCard />
         <div className = {styles.socialIcons}>
